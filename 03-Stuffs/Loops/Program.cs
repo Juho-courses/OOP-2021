e313c0f739dcddf17a386c1b945cb3a6774d0214
Juho Salli <juho.salli@gmail.com>
2021-02-02 11:08:19 +0200

    Add the codes for lecture 03

---
+++ 03-Stuffs/Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = 5;
+            // i++ == i = i + 1
+            // for (init ; condition ; growing )
+            // for i in range(0, end):
+            for (int i = 0; i < end; i++) {
+                Console.WriteLine(i);
+            }
+
+            int rounds = 0;
+            while (rounds < 0) {
+                Console.WriteLine("while " + rounds.ToString());
+                rounds++; // round = rounds + 1
+            }
+            Console.WriteLine("after while");
+
+            rounds = 0;
+            do {
+                Console.WriteLine("do-while " + rounds.ToString());
+                rounds++;
+            } while (rounds < 0);
+
+            
+        }
+    }
+}
---
+++ 03-Stuffs/ReadingUserInput/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ReadingUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a number: ");
+            string input = Console.ReadLine();
+            
+            Console.WriteLine(input);
+
+            int inputInt = int.Parse(input);
+
+            Console.WriteLine(inputInt * 2);
+
+
+            Console.Write("Give a number: ");
+            string input2 = Console.ReadLine();
+
+            double inputDouble = double.Parse(input2);
+            
+            int d2 = (int)7.2;
+
+            Console.WriteLine(d2);
+
+            int sInt = 2;
+            string ssInt = sInt.ToString();
+
+            double d3 = 23.12;
+            d3.ToString();
+        }
+    }
+}
