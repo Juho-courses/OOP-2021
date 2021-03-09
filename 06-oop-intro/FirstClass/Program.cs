217d22a3adc17080a5afcc53bfa9374fd50f35c8
Juho Salli <juho.salli@gmail.com>
2021-03-09 11:51:06 +0200

    Add the code related to the OOP intro lecture

---
+++ 06-oop-intro/FirstClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.Greet("Juho");
+
+        }
+    }
+
+    class MyClass
+    {
+        public void Greet(string str)
+        {
+            Console.WriteLine("Hello " + str);
+        }
+    }
+}
