bf60ff08e3a7d65d18f68db6139264a6a6e8778c
Juho Salli <juho.salli@gmail.com>
2021-03-23 11:36:08 +0200

    Code for the lecture about class members

---
+++ 08-oop-static-members/ClassMembers/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ClassMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Animal(4);
+
+            // dog.MakeSound();
+            // Not OK
+            // dog.Count;
+            Console.WriteLine(Animal.Count);
+
+            Animal spider = new Animal(8, 8);
+            // spider.MakeSound();
+            Console.WriteLine(Animal.Count);
+
+            Animal.StaticAnimal();
+
+            Console.WriteLine(AnimalStats.CalculateEyesToLegsRatio(spider));
+            Console.WriteLine(AnimalStats.CalculateEyesToLegsRatio(dog));
+
+        }
+    }
+
+    static class AnimalStats
+    {
+        // doesn't work, because instance member
+        // private int count = 0;
+        public static float CalculateEyesToLegsRatio(Animal animal)
+        {
+            return (float)animal.EyeCount / animal.LegCount;
+        }
+    }
+
+    class Animal
+    {
+        // instance members
+        public int LegCount { get; private set; }
+        public int EyeCount { get; private set; }
+
+        // class member
+        public static int Count { get; private set; } = 0;
+
+        public Animal(int legs, int eyes = 2)
+        {
+            LegCount = legs;
+            EyeCount = eyes;
+            Count = Count + 1;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Animal sounds!");
+        }
+
+        public static void StaticAnimal()
+        {
+            Console.WriteLine("StaticAnimal() called");
+        }
+    }
+}
---
+++ 08-oop-static-members/ReadingFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("./data.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    break;
+                }
+            }
+        }
+    }
+}
